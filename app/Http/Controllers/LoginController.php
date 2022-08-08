7814b8fcac2b96c0520825166603c529e6f33b68
YanVl <yanvitor.gti@gmail.com>
2022-08-08 12:28:04 -0300

    Validados os usuarios no bd

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class LoginController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('site.login', ['titulo' => 'Login']);
     }
 
-    public function autenticar(Request $request) {
+    public function autenticar(Request $request)
+    {
 
         //regras de validação
         $regras = [
@@ -27,7 +30,25 @@ class LoginController extends Controller
         //se não passar pelo validate
         $request->validate($regras, $feedback);
 
-        print_r($request->all());
+        //recuperados os parametros do formulário
+        $email = $request->get('usuario');
+        $password = $request->get('senha');
 
+        echo "Usuário: $email | Senha: $password";
+        echo '<br>';
+
+        //iniciar o model User
+        $user = new User();
+
+        $usuario = $user->where('email', $email)
+            ->where('password', $password)
+            ->get()
+            ->first();
+
+        if(isset($usuario->name)) {
+            echo 'Usuário existe';
+        } else {
+            echo 'Usuário não existe';
+        }
     }
 }
