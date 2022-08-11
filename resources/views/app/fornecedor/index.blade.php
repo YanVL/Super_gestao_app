55d7cefc0e6cce6b25b71df4b1bd5975b49172b9
YanVl <yanvitor.gti@gmail.com>
2022-08-10 23:55:02 -0300

    Protecao acesso ao app implementada

--- resources/views/app/fornecedor/index.blade.php
+++
@@ -1,5 +0,0 @@
-<h3>Fornecedor</h3> 
-
-@php
-  
-@endphp
--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,11 @@ Route::get('/login/{erro?}', [\App\Http\Controllers\LoginController::class, 'ind
 Route::post('/login', [\App\Http\Controllers\LoginController::class, 'autenticar'])->name('site.login');
 
 Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(function () {
-    Route::get('/clientes', function () {
-        return 'clientes';
-    })->name('app.clientes');
-    Route::get('/fornecedores', [\App\Http\Controllers\FornecedorController::class, 'index'])->name('app.fornecedores');
-    Route::get('/produtos', function () {
-        return 'produtos';
-    })->name('app.produtos');
+    Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('app.home');
+    Route::get('/sair', [\App\Http\Controllers\LoginController::class, 'sair'])->name('app.sair');
+    Route::get('/cliente', [\App\Http\Controllers\ClienteController::class, 'index'])->name('app.cliente');
+    Route::get('/fornecedor', [\App\Http\Controllers\FornecedorController::class, 'index'])->name('app.fornecedor');
+    Route::get('/produto', [\App\Http\Controllers\ProdutoController::class, 'index'])->name('app.produto');
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
