48fb6f314afb88b831860b3c757369985cb85b9a
YanVl <yanvitor.gti@gmail.com>
2022-08-24 01:54:36 -0300

    Adicionado o form de exclusão

--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -40,16 +40,25 @@
                                 <td>{{ $produto->descricao }}</td>
                                 <td>{{ $produto->peso }}</td>
                                 <td>{{ $produto->unidade_id }}</td>
-                                <td><a href=" {{ route('produto.show', ['produto' => $produto->id ]) }} ">Visualizar</a></td>
-                                <td><a href="">Excluir</a></td>
-                                <td><a href="">Editar</a></td>
+                                <td><a href="{{ route('produto.show', ['produto' => $produto->id]) }}">Visualizar</a></td>
+                                <td>
+                                    <form id="form_{{ $produto->id }}" method="post"
+                                        action="{{ route('produto.destroy', ['produto' => $produto->id]) }}">
+                                        @method('DELETE')
+                                        @csrf
+                                        
+                                        <a href="#"
+                                            onclick="document.getElementById('form_{{ $produto->id }}').submit()">Excluir</a>
+                                    </form>
+                                </td>
+                                <td><a href="{{ route('produto.edit', ['produto' => $produto->id]) }}">Editar</a></td>
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
 
                 {{ $produtos->appends($request)->links() }}
-                
+
             </div>
         </div>
 
